ef67106b97f5dcacafbacc1752312b90baac6f90
Yaozhiguo <1224853899@qq.com>
2023-04-12 12:48:33 +0800

    feat: add exports module in mbg

--- mbg/src/main/java/module-info.java
+++ mbg/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module cn.gov.yrcc.mbg {
-	requires java.sql;
-	requires mysql.connector.java;
+	exports cn.gov.yrcc;
+	exports cn.gov.yrcc.model;
+	exports cn.gov.yrcc.mapper;
+
 	requires org.mybatis.generator;
 	requires org.mybatis;
+
+	requires java.sql;
+	requires mysql.connector.java;
 	requires swagger.annotations;
-	exports cn.gov.yrcc;
 }
